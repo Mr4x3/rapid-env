7c8c5164b5ec338b500363ad554770d3e33142ec
Mr4x3 <Vivek@crushus.com>
2016-09-11 15:06:43 +0000

    From Raspberry Pi3

---
+++ new_machine_env.sh
@@ -0,0 +1,11 @@
+rm ~/.bashrc
+ln env/.bashrc .bashrc
+git config --global user.email "Vivek@crushus.com"
+git config --global user.name "Mr4x3"
+git config color.ui true
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+sudo apt install ranger
+sudo apt install glances
+
