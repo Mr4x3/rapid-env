949e20254b2e7f89bd532fbca0ae6da42252825f
Mr4x3 <Vivek@crushus.com>
2016-11-30 09:35:00 +0530

    Present Directry Variable

--- new_machine_env.sh
+++ new_machine_env.sh
@@ -1,10 +1,13 @@
 rm ~/.bashrc
-ln ~/env/.bashrc ~/.bashrc
+# Present Directry
+present_directry=./
+ln $present_directry.bashrc ~/.bashrc
 git config --global user.email "Vivek@crushus.com"
 git config --global user.name "Mr4x3"
 git config color.ui true
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.st "status"
+git config --global core.excludesfile $present_directry.gitignore
 
 # Packages Install
 sudo apt install -y ssh python3-pip htop chromium-browser glances tree ranger xclip aptitude tmux indicator-multiload indicator-cpufreq mysql-server
